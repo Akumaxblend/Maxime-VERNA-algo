f4dfff140302f8e801babfe57f8414bff46fac50
Akumaxblend <83503168+Akumaxblend@users.noreply.github.com>
2023-03-01 11:02:58 +0100

    Fix exo 5

--- TP1/exo5.cpp
+++ TP1/exo5.cpp
@@ -2,11 +2,12 @@
 #include <QApplication>
 #include <time.h>
 
-void complexSquare(Point * z){
+Point complexSquare(Point z){
 
-    z->x = z->x * z->x - z->y * z->y;
-
-    z->y = 2 * z->x * z-> y;
+    Point tmp;
+    tmp.x = z.x * z.x - z.y * z.y;
+    tmp.y = 2 * z.x * z. y;
+    return tmp;
 }
 int isMandelbrot(Point z, int n, Point point){
     // recursiv Mandelbrot
@@ -18,22 +19,12 @@ int isMandelbrot(Point z, int n, Point point){
     // otherwise, process the square of z and recall
     // isMandebrot
 
-    if(n == 0){
-
-        if (z.length() > 2){
-
-            return n;
-        }
-
-        else return 0;
-    }
-
-    if(z.length() > 2){
+    if(z.length() > 2 || n == 0){
 
         return n;
     }
 
-    complexSquare(&z);
+    z = complexSquare(z);
     z.x += point.x;
     z.y += point.y;
     return isMandelbrot(z, n - 1, point);
