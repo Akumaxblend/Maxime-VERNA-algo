9f0a5966c143b5338a993a0389c27a3f07d4fc7b
Akumaxblend <83503168+Akumaxblend@users.noreply.github.com>
2023-03-19 21:05:08 +0100

    avancees tp4

--- TP4/exo1.cpp
+++ TP4/exo1.cpp
@@ -11,24 +11,66 @@ using std::string;
 
 int Heap::leftChildIndex(int nodeIndex)
 {
-    return 0;
+    return (nodeIndex * 2) + 1;
 }
 
 int Heap::rightChildIndex(int nodeIndex)
 {
-    return 0;
+    return (nodeIndex * 2) + 2;
 }
 
 void Heap::insertHeapNode(int heapSize, int value)
 {
-	// use (*this)[i] or this->get(i) to get a value at index i
+    //use (*this)[i] or this->get(i) to get a value at index i
+
+	int tmp;
+
 	int i = heapSize;
+	(*this)[i] = value;
+
+	while(i > 0 && this->get(i) > this->get((i-1) / 2)){
+
+		tmp = this->get(i);
+
+		(*this)[i] = this->get((i-1)/2);
+		(*this)[(i-1)/2] = tmp;
+
+		i = (i-1) / 2;
+
+	}
 }
+
 
 void Heap::heapify(int heapSize, int nodeIndex)
 {
 	// use (*this)[i] or this->get(i) to get a value at index i
 	int i_max = nodeIndex;
+
+	if(leftChildIndex(nodeIndex) <heapSize){
+
+		if((*this)[leftChildIndex(nodeIndex)] > (*this)[i_max]){
+
+			i_max = leftChildIndex(nodeIndex);
+	}
+	
+
+	}if(rightChildIndex(nodeIndex) < heapSize){
+
+		if ((*this)[rightChildIndex(nodeIndex)] > (*this)[i_max]){
+
+			i_max = rightChildIndex(nodeIndex);
+		}
+	}	
+
+	if(i_max != nodeIndex){
+
+		int tmp = (*this)[nodeIndex];
+		(*this)[nodeIndex] = (*this)[i_max];
+		(*this)[i_max] = tmp;
+
+		heapify(heapSize, i_max);
+	}
+	else return;
 }
 
 void Heap::buildHeap(Array& numbers)
