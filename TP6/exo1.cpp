48316cb02d21f55dc3fcdd1989963e93f746636f
Akumaxblend <83503168+Akumaxblend@users.noreply.github.com>
2023-04-14 10:51:02 +0200

    Avancees TP6

--- TP6/exo1.cpp
+++ TP6/exo1.cpp
@@ -12,6 +12,25 @@ void Graph::buildFromAdjenciesMatrix(int **adjacencies, int nodeCount)
 	  * this->appendNewNode
 	  * this->nodes[i]->appendNewEdge
 	  */
+
+	 for(int i = 0; i<nodeCount; i++){
+
+		GraphNode * toInsert = new GraphNode(i);
+		this->appendNewNode(toInsert);
+
+	 }
+
+	 for(int i = 0; i<nodeCount; i++){
+
+		for(int j = 0; j<nodeCount; j++){
+			
+			if(adjacencies[i][j] != 0){
+
+				this->nodes[i]->appendNewEdge(this->nodes[j], adjacencies[i][j]);
+
+			}
+		}
+	 }
 }
 
 void Graph::deepTravel(GraphNode *first, GraphNode *nodes[], int &nodesSize, bool visited[])
@@ -20,7 +39,20 @@ void Graph::deepTravel(GraphNode *first, GraphNode *nodes[], int &nodesSize, boo
 	  * Fill nodes array by travelling graph starting from first and using recursivity
 	  */
 
-}
+
+	nodes[nodesSize] = first;
+	visited[first->value] = true;
+	nodesSize ++;
+
+	for(Edge * e = first->edges; e != nullptr; e = e->next){
+
+		if(!visited[e->destination->value]){
+			deepTravel(e->destination, nodes, nodesSize, visited);
+		}
+
+	}
+
+} 
 
 void Graph::wideTravel(GraphNode *first, GraphNode *nodes[], int &nodesSize, bool visited[])
 {
@@ -33,6 +65,23 @@ void Graph::wideTravel(GraphNode *first, GraphNode *nodes[], int &nodesSize, boo
 	 */
 	std::queue<GraphNode*> nodeQueue;
 	nodeQueue.push(first);
+
+	while(!nodeQueue.empty()){
+
+		GraphNode * node = nodeQueue.front();
+		nodeQueue.pop();
+		nodes[nodesSize] = node;
+		visited[node->value] = true;
+		nodesSize ++;
+
+		for(Edge * e = node->edges ; e != nullptr ; e = e->next){
+
+			if(!visited[e->destination->value]){
+
+				nodeQueue.push(e->destination);
+			}
+		}
+	}
 }
 
 bool Graph::detectCycle(GraphNode *first, bool visited[])
@@ -42,7 +91,31 @@ bool Graph::detectCycle(GraphNode *first, bool visited[])
 	  (the first may not be in the cycle)
 	  Think about what's happen when you get an already visited node
 	**/
-    return false;
+
+	// if(visited[first->value]){
+
+	// 	return true;
+	// }
+
+	if(first == nullptr){
+
+		return false;
+
+	}
+
+	
+
+	while(first->edges != nullptr){
+
+		if(visited[first->value]){
+
+			return true;
+		}
+		visited[first->value] = true;
+
+		detectCycle(first->edges->destination, visited);
+
+	}
 }
 
 int main(int argc, char *argv[])
